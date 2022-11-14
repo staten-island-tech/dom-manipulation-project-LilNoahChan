78a1ba6d93de1f0a4a29240e417ba9699dda3031
LilNoahChan <noahchan305@gmail.com>
2022-11-14 13:14:29 -0500

    11/14

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const DOMSelectors = {
   imgURL: document.getElementById("imgURL"),
   create: document.getElementById("create"),
   container: document.getElementById("container"),
-  temp: document.querySelector("display"),
+  display: document.querySelector("display"),
   remove: document.getElementById("remove"),
 };
 
@@ -19,11 +19,11 @@ DOMSelectors.create.addEventListener("click", function () {
 
   DOMSelectors.container.insertAdjacentHTML(
     "afterbegin",
-    `<div class="temp">
+    `<div class="display">
         <image src></image>
-        <h1 class="song">${song}</h1>
-        <h2 class="art">${artist}</h2>
+        <h1 class="song">Title: ${song}</h1>
         <img src="${img}" class="myImage"alt="">
+        <h2 class="art">Song By:${artist}</h2>
         <button id="remove" onclick="this.parentElement.remove()">Delete</button>
     </div>`
   );
