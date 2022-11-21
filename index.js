d304e93d77d2c8ba2af665b838b989cb73c293bf
LilNoahChan <noahchan305@gmail.com>
2022-11-21 13:15:04 -0500

    dne?

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const DOMSelectors = {
   songName: document.getElementById("songName"),
   artistName: document.getElementById("artistName"),
@@ -7,22 +5,35 @@ const DOMSelectors = {
   create: document.getElementById("create"),
   container: document.getElementById("container"),
   display: document.querySelector("display"),
-  remove: document.getElementById("remove"),
 };
 
-DOMSelectors.create.addEventListener("click", function () {
+DOMSelectors.create.addEventListener("click", function (event) {
+  event.preventDefault();
   let input1 = DOMSelectors.songName.value;
   let input2 = DOMSelectors.artistName.value;
   let input3 = DOMSelectors.imgURL.value;
+  //mover functions outside
+  function makeCard() {
+    DOMSelectors.container.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="display">
+          <image src></image>
+          <h1 class="song">Title: ${input1}</h1>
+          <img src="${input3}" class="myImage"alt="">
+        <h2 class="artist">Song By:${input2}</h2>
+        <button class="remove">Delete</button>
+      </div>`
+    );
+  }
+  function erase() {
+    let removes = document.querySelectorAll(".remove");
+    removes.forEach((eachRemove) => {
+      eachRemove.addEventListener("click", (event) => {
+        event.target.parentElement.remove();
+      });
+    });
+  }
 
-  DOMSelectors.container.insertAdjacentHTML(
-    "afterbegin",
-    `<div class="display">
-        <image src></image>
-        <h1 class="song">Title: ${input1}</h1>
-        <img src="${input3}" class="myImage"alt="">
-      <h2 class="artist">Song By:${input2}</h2>
-        <button id="remove" onclick="this.parentElement.remove()">Delete</button>
-    </div>` //whoops
-  );
+  makeCard();
+  erase();
 });
